48433206e5c5e29aca8ac2e5e588f6ac0b4545ed
vinicios <vinicioshenriques@hotmail.com>
2019-08-28 23:40:20 -0300

    Agendamento e notificações criados, e alertas por email

---
+++ modulo02/src/config/mail.js
@@ -0,0 +1,25 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'aa35fd5037930a',
+    pass: '17294eeb097e6a',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+// export default {
+//   host: 'smtp.gmail.com',
+//   port: 465,
+//   secure: true,
+//   auth: {
+//     user: 'viniciosdelabari@gmail.com',
+//     pass: 'qwe321qwe',
+//   },
+//   default: {
+//     from: 'Equipe GoBarber <noreply@gobarber.com>',
+//   },
+// };
