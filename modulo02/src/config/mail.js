29b268434a366882ad6d78bdaecdfb05d67ad2c0
vinicios <vinicioshenriques@hotmail.com>
2019-08-30 00:42:03 -0300

    Adicionada fila com redis para envio de emails, listagem de horários disponíveis, adição de campos virtuais no postgress para informações adicionais, tratamento de exceções e variáveis de ambiente

--- modulo02/src/config/mail.js
+++ modulo02/src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'aa35fd5037930a',
-    pass: '17294eeb097e6a',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
