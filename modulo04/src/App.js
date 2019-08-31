dbe3b55c084aa76e2976c7d2fac5ea3efc6f0f5f
vinicios <vinicioshenriques@hotmail.com>
2019-08-31 15:47:44 -0400

    Modulo 4 - iniciando react finalizado

--- modulo04/src/App.js
+++ modulo04/src/App.js
@@ -2,8 +2,10 @@ import React from "react";
 
 import "./App.css";
 
+import TechList from "./components/TechList";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return <TechList />;
 }
 
 export default App;
---
+++ modulo04/src/components/TechList.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+
+import TechItem from "./TechItem";
+
+class TechList extends Component {
+  state = {
+    newTech: "",
+    techs: []
+  };
+
+  //é chamado assim que o componente aparece em tela, como componente que busca
+  //dados em api externa, pode ser chamado por esta função
+  componentDidMount() {
+    const techs = localStorage.getItem("techs");
+
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs) });
+    }
+  }
+
+  // Executado sempre que houver alteração nas props ou no estado do componente, ele recebe as
+  // propriedades e estados antigos antes da modificação, assim podemos fazer comparações
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.techs !== this.state.techs) {
+      localStorage.setItem("techs", JSON.stringify(this.state.techs));
+    }
+  }
+
+  // executado quando o componente deixa de existir
+  componentWillUnmount() {}
+
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: ""
+    });
+  };
+
+  handleDelete = tech => {
+    this.setState({
+      techs: this.state.techs.filter(t => t !== tech)
+    });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map(tech => (
+            <TechItem
+              key={tech}
+              tech={tech}
+              onDelete={() => this.handleDelete(tech)}
+            />
+          ))}
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        />
+        <button type="submit">Enviar</button>
+      </form>
+    );
+  }
+}
+
+export default TechList;
