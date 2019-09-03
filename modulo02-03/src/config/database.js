5410670f8d69190398aa7e96d5f5b151849b66a0
vinicios <vinicioshenriques@hotmail.com>
2019-09-03 08:48:28 -0300

    navigation instalado e configurado

--- modulo02-03/src/config/database.js
+++ modulo02-03/src/config/database.js
@@ -5,7 +5,7 @@ module.exports = {
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.NAME,
+  database: process.env.DB_NAME,
   define: {
     timestamps: true,
     underscored: true,
---
+++ modulo06/src/routes.js
@@ -0,0 +1,44 @@
+// no react native adicionar as linhas extras do gesture handler no mainActivity.java
+
+// import com.facebook.react.ReactActivityDelegate;
+// import com.facebook.react.ReactRootView;
+// import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
+// @Override
+//     protected ReactActivityDelegate createReactActivityDelegate() {
+//     return new ReactActivityDelegate(this, getMainComponentName()) {
+//     @Override
+//     protected ReactRootView createRootView() {
+//         return new RNGestureHandlerEnabledRootView(MainActivity.this);
+//     }
+//     };
+// }
+
+import {
+    createAppContainer,
+    createStackNavigator,
+} from 'react-navigation-stack';
+
+import Main from './pages/Main';
+import User from './pages/User';
+
+const Routes = createAppContainer(
+    createStackNavigator(
+        {
+            Main,
+            User,
+        },
+        {
+            headerLayoutPreset: 'center',
+            headerBackTitleVisible: false,
+            defaultNavigationOptions: {
+                headerStyle: {
+                    backgroundColor: '#7159c1',
+                },
+                headerTintColor: '#Fff',
+            },
+        }
+    )
+);
+
+export default Routes;
