eb573ecb4bf2a77a6ff863d075a6342771efcbeb
vinicios <vinicioshenriques@hotmail.com>
2019-09-06 00:27:21 -0300

    Inicio módulo 07 - React com Redux

---
+++ modulo07/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
+import GlobalStyle from './styles/global';
+import Routes from './routes';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes />
+      <GlobalStyle />
+    </BrowserRouter>
+  );
+}
+
+export default App;
