b75a3fcbb2db7ed5ff1c3e624d2f0664079ab661
vinicios <vinicioshenriques@hotmail.com>
2019-09-08 19:20:30 -0300

    adicionado redux saga

--- modulo07/src/config/ReactotronConfig.js
+++ modulo07/src/config/ReactotronConfig.js
@@ -1,11 +1,13 @@
 import Reactotron from 'reactotron-react-js';
 import { reactotronRedux } from 'reactotron-redux';
+import reactotronSaga from 'reactotron-redux-saga';
 
 if (process.env.NODE_ENV === 'development') {
   const tron = Reactotron.configure({
     host: '192.168.1.156',
   })
     .use(reactotronRedux())
+    .use(reactotronSaga())
     .connect();
 
   tron.clear();
--- modulo07/src/pages/Home/index.js
+++ modulo07/src/pages/Home/index.js
@@ -25,9 +25,9 @@ class Home extends Component {
     this.setState({ products: data });
   }
 
-  handleAddProduct = product => {
-    const { addToCart } = this.props;
-    addToCart(product);
+  handleAddProduct = id => {
+    const { addToCartRequest } = this.props;
+    addToCartRequest(id);
   };
 
   render() {
@@ -43,7 +43,7 @@ class Home extends Component {
             <span>{product.priceFormatted}</span>
             <button
               type="button"
-              onClick={() => this.handleAddProduct(product)}
+              onClick={() => this.handleAddProduct(product.id)}
             >
               <div>
                 <MdAddShoppingCart size={16} color="#fff" />{' '}
