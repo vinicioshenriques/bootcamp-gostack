750baf033c42d74a734218c41c43593fb00c2879
vinicios <vinicioshenriques@hotmail.com>
2019-09-10 08:44:04 -0300

    modulo 08 iniciado, eslint, prettier e editorConfig iniciados, useState criados

---
+++ modulo08/src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [tech, setTech] = useState(['ReactJS', 'React Native']);
+  const [newTech, setNewTech] = useState('');
+
+  function handleAdd() {
+    setTech([...tech, newTech]);
+    setNewTech('');
+  }
+
+  return (
+    <>
+      <ul>
+        {tech.map(t => (
+          <li key={t}>{t}</li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        value={newTech}
+        onChange={e => setNewTech(e.target.value)}
+      />
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
+    </>
+  );
+}
+
+export default App;
