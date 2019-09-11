d3d884126318eff7da0d794970f637c5b10e3e94
vinicios <vinicioshenriques@hotmail.com>
2019-09-11 08:53:21 -0300

    modificações necessárias na api para prosseguir com a parte web

--- modulo02-03/src/app/controllers/UserController.js
+++ modulo02-03/src/app/controllers/UserController.js
@@ -1,5 +1,6 @@
 import * as Yup from 'yup';
 import User from '../models/User';
+import File from '../models/File';
 
 class UserController {
   async store(req, res) {
@@ -56,8 +57,6 @@ class UserController {
 
     const user = await User.findByPk(req.userId);
 
-    console.log(user);
-
     if (email !== user.email) {
       const userExists = await User.findOne({
         where: { email },
@@ -71,13 +70,24 @@ class UserController {
     if (oldPassword && !(await user.checkPassword(oldPassword))) {
       return res.status(401).json({ error: 'Password does not match' });
     }
-    const { id, name, provider } = await user.update(req.body);
+
+    await user.update(req.body);
+
+    const { id, name, avatar } = await User.findByPk(req.userId, {
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
 
     return res.json({
       id,
       name,
       email,
-      provider,
+      avatar,
     });
   }
 }
