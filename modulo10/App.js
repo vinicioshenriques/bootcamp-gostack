c773da97e2c0337a51b28ae108e258417bd28c48
vinicios <vinicioshenriques@hotmail.com>
2019-09-15 23:07:49 -0300

    eslint e prettier configurados

---
+++ modulo10/App.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment} from 'react';
+import {SafeAreaView, View, Text, StatusBar} from 'react-native';
+
+const App = () => {
+  return (
+    <Fragment>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <View>
+          <Text>Hello World</Text>
+        </View>
+      </SafeAreaView>
+    </Fragment>
+  );
+};
+
+export default App;
