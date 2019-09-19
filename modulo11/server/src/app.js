66a6584f01523315d35e54b788d90ce1a0e382e9
vinicios <vinicioshenriques@hotmail.com>
2019-09-19 08:50:40 -0300

    sqlite e variáveis de ambiente adicionadas ao teste

--- modulo11/server/src/app.js
+++ modulo11/server/src/app.js
@@ -1,5 +1,4 @@
-import 'dotenv/config';
-
+import './bootstrap';
 import Youch from 'youch';
 import express from 'express';
 import 'express-async-errors';
@@ -8,6 +7,7 @@ import routes from './routes';
 
 // Uncomment this line to enable database access
 // --------
+
 import './database';
 
 class App {
