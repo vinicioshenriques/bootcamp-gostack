dfc84df040cab004ce43794ef5158eb7863598d5
vinicios <vinicioshenriques@hotmail.com>
2019-09-20 21:26:41 -0300

    inicio dos testes com redux

---
+++ modulo12/src/__tests__/components/TechListRedux.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { render, fireEvent, cleanup } from "@testing-library/react";
+import TechList from "~/components/TechListRedux";
+
+describe("TechListRedux component", () => {
+  it("should sum 1 plus 1 and return 2", () => {
+    expect(1 + 1).toBe(2);
+  });
+});
